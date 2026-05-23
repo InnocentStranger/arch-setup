b33853012a711c53b07ed6b265048172eba8706f
aryan.rajput <aryancoder440@gmail.com>
2026-05-23 19:07:31 +0530

    added trouble.nvim

---
+++ test/nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,46 @@
+-- lua/plugins/trouble.lua
+
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-mini/mini.icons" },
+  cmd = "Trouble",
+  keys = {
+    -- workspace diagnostics
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    -- buffer diagnostics only
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    -- document symbols
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    -- LSP definitions, references, implementations
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP (Trouble)",
+    },
+    -- location list
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    -- quickfix list
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+  opts = {},
+}
