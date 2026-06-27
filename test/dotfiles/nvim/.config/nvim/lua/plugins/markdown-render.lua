b9e7c99c889c4b1821dcdaa31127dd145702bec2
aryan.rajput <aryan.rajput@git>
2026-06-27 08:06:20 +0530

    removed markdown renderer

--- test/dotfiles/nvim/.config/nvim/lua/plugins/markdown-render.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-mini/mini.icons" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    preset = "none",
-  },
-}
