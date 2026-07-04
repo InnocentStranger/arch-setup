f22741380ef47f431fb1c1c163cc14c06a8e87a8
aryan.rajput <aryan.rajput@git>
2026-07-04 14:36:56 +0530

    added hyprland config

---
+++ hyprland-config/dotfiles/hypr/.config/hypr/hyprland.lua
@@ -0,0 +1,9 @@
+-- hyprland.lua
+
+require("modules.monitor")
+require("modules.programs")
+require("modules.layout")
+require("modules.animation")
+require("modules.gesture")
+require("modules.input")
+require("modules.keybind")
