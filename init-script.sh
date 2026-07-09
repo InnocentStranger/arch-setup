c58119a091f8ae351e7dd9ae869dd242e478fc98
aryan.rajput <aryan.rajput@gmail.com>
2026-07-09 23:43:54 +0530

    enabled walker, elephant and waybar in init script

--- init-script.sh
+++ init-script.sh
@@ -154,6 +154,9 @@ systemctl --user daemon-reload
 systemctl --user enable --now hyprpolkitagent.service
 systemctl --user enable --now hypridle.service
 systemctl --user enable --now hyprpaper.service
+systemctl --user enable --now elephant.service
+systemctl --user enable --now walker.service
+systemctl --user enable --now waybar.service
 
 mkdir -p ~/Pictures/Screenshots/
 
