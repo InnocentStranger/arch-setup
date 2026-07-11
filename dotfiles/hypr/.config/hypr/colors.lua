e834f904956779c8895c6b294fca344c9abf6dbf
aryan.rajput <aryan.rajput@gmail.com>
2026-07-11 15:43:13 +0530

    gtk4 live theme switching working

--- dotfiles/hypr/.config/hypr/colors.lua
+++ dotfiles/hypr/.config/hypr/colors.lua
@@ -1,8 +1,8 @@
 -- Matugen generated colors for Hyprland
 return {
-  primary = "#b7c4ff",
-  surface = "#121318",
-  surface_variant = "#45464f",
-  primary_container = "#364479",
-  on_primary = "#1e2d61",
+  primary = "#abd290",
+  surface = "#11140e",
+  surface_variant = "#43483e",
+  primary_container = "#2e4f1b",
+  on_primary = "#183706",
 }
