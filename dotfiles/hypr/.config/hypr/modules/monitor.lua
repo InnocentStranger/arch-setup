8cbce9e8e9dd4d3b709484586597f8746a9118ab
aryan.rajput <aryancoder440@gmail.com>
2026-07-11 20:38:50 +0530

    enabled vrr by default and commented 2nd monitor setting

--- dotfiles/hypr/.config/hypr/modules/monitor.lua
+++ dotfiles/hypr/.config/hypr/modules/monitor.lua
@@ -3,7 +3,8 @@ local primary_monitor = {
   mode = "preferred",
   position = "auto",
   scale = "auto",
+  vrr = 1,
 }
 
 hl.monitor(primary_monitor)
-hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1", default = true })
+-- hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1", default = true })
